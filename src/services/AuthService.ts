3ca7d2a9576a03190e178736f7d742dd29f6127f
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-10 20:25:37 -0400

    jwt service

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,13 +1,30 @@
 import { compare, hash } from "bcrypt";
+import jwt from "jsonwebtoken";
+import { IUser } from "modules/user/IUserRepo";
 
+interface IUserToken {
+    id: number;
+    name: string;
+    email: string;
+}
 
 export interface IAuthServices {
+    generateUserToken(user: IUserToken): string;
+    decodeUserToken(token: string): IUserToken;
     hashPassword(password: string): Promise<string>;
     compare(userPassword: string, hashedPassword: string): Promise<boolean>;
 }
 
 export class AuthServices implements IAuthServices {
     constructor() { }
+
+    public generateUserToken(user: IUserToken) {
+        return jwt.sign(user, `userDetails`, { expiresIn: '1h' });
+    }
+
+    public decodeUserToken(token: string): any {
+        return jwt.verify(token, `userDetails`);
+    }
 
     public hashPassword(password: string) {
         return hash(password, 12);
