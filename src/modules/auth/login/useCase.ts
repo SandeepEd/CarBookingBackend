c419698a08888e0cf065badded1b1776be6cf9ea
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-10 20:23:39 -0400

    jwt token

--- src/modules/auth/login/useCase.ts
+++ src/modules/auth/login/useCase.ts
@@ -1,11 +1,11 @@
-import { AuthServices, IAuthServices } from "../../../services";
-import { IUseCase } from "../../../shared/IUseCase";
+import { IAuthServices } from "../../../services";
 import { IUser } from "../../user/IUserRepo";
 import { UserRepository } from "../../user/userRepository";
+import { Request } from "express";
 
 export class LogInUseCase {
     constructor(private authServices: IAuthServices, private userRepo: UserRepository) { }
-    async execute(data: Pick<IUser, 'email' | 'password'>): Promise<any> {
+    async execute(data: Pick<IUser, 'email' | 'password'>, req: Request): Promise<any> {
         const { password, email } = data;
         const user = await this.userRepo.getUserByEmail(email);
         if (!user) {
@@ -17,7 +17,11 @@ export class LogInUseCase {
         if (!isPasswordValid) {
             throw new Error("Invalid password");
         }
-
-        return user;
+        const token = this.authServices.generateUserToken({
+            id: user.id,
+            name: user.name,
+            email: user.email
+        });
+        return { user, token };
     }
 } 
