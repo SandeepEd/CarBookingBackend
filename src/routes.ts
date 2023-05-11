2a16ca5806ba3041440a3cf7a35f0ee52299688c
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-10 20:20:43 -0400

    new middle ware

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import { carBookingRoutes } from "./modules/cars/routes";
 import { logInRouter, signUpRouter } from "./modules/auth/routes";
+import { ManageRoute } from "./utils/ManageRoute";
+import { userRouter } from "./modules/user/routes";
+console.log(ManageRoute);
 
 const router = express.Router();
-router.use('/cars', carBookingRoutes);
+router.use('/cars', ManageRoute, carBookingRoutes);
 router.use('/sign-up', signUpRouter)
 router.use(`/login`, logInRouter)
+router.use('/user', ManageRoute, userRouter)
 
 export { router }
