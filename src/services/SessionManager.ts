d3bc47ff27b4d5ef65a10159a128e2466d44b54b
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-14 19:47:29 -0400

    ..

--- src/services/SessionManager.ts
+++ src/services/SessionManager.ts
@@ -1,3 +1,4 @@
+import { PreconditionFailed } from "http-errors";
 export class SessionManager {
     static setSession(req: any, token: string) {
         req.session.token = token;
@@ -12,9 +13,9 @@ export class SessionManager {
     }
 
     static checkSession(req: any, res: any) {
-        if (!req.session.token) {
+        if (!req.session || !req.session.token) {
             res.cookie(`spa_token`, false);
-            throw new Error(`Invalid or no session`);
+            throw new PreconditionFailed(`Invalid or no session`);
         }
         return req.session.token;
     }
