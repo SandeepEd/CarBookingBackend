b95581d54cbc384363e1036f755e83f7c966ed34
Sandeep Edara <edarasp@mail.uc.edu>
2023-05-14 19:39:56 -0400

    add routes

--- src/modules/checkout/routes.ts
+++ src/modules/checkout/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { addToCartController } from "./useCases/addToCart";
 import { deleteCheckoutItemController } from "./useCases/deleteCart";
+import { getCheckoutItemsController } from "./useCases/getFromCart";
 
 const checkoutRoute = Router();
 
-checkoutRoute.post('/:carId', addToCartController.handleController);
+checkoutRoute.post('/add/:carId', addToCartController.handleController);
 checkoutRoute.delete('/:id', deleteCheckoutItemController.handleController);
+checkoutRoute.get('/', getCheckoutItemsController.handleController);
 
 export { checkoutRoute }
